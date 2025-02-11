4b6829c4bb3d8d775c42e3d85ebbdc60dcd79455
braydenbos <bosbrayden@gmail.com>
2025-02-11 15:40:29 +0100

    Added user data

---
+++ Assets/Scripts/User/UserData.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+using ScriptableObjects;
+using UnityEngine;
+using Util;
+
+namespace User
+{
+    public class UserData : Singleton<UserData>
+    {
+        private static int netWorth; // The amount of currency the player has
+        public int NetWorth => netWorth;
+
+        private List<ItemScriptableObject> _inventory; // A list of Items that the player has
+        public List<ItemScriptableObject> Inventory => _inventory;
+        
+        /// <summary>
+        /// Changes the current net worth by adding "change" to it
+        /// </summary>
+        /// <param name="change"> Amount to add to netWorth</param>
+        public void ChangeNetWorth(int change) => netWorth += change;
+        
+        /// <summary>
+        /// Add "item" to Inventory
+        /// </summary>
+        /// <param name="item">Item to add</param>
+        public void AddItemToInventory(ItemScriptableObject item) => _inventory.Add(item);
+
+        /// <summary>
+        /// Remove "item" to Inventory
+        /// </summary>
+        /// <param name="item">Item to remove</param>
+        public bool RemoveItemFromInventory(ItemScriptableObject item)
+        {
+            if (!_inventory.Contains(item)) return false;
+            _inventory.Remove(item);
+            return true;
+        }
+    }
+}
