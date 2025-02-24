ba9b341787647b95ceb8449f2ba6e3b91b2dd8b0
braydenbos <bosbrayden@gmail.com>
2025-02-24 14:22:53 +0100

    comments[DayLoopEvents]: added comments to the DayLoopEvents manager and DayLoopListeners

--- Assets/Scripts/DayLoop/DayLoopListeners.cs
+++ Assets/Scripts/DayLoop/DayLoopListeners.cs
@@ -4,19 +4,29 @@ namespace DayLoop
 {
     public abstract class DayLoopListeners : MonoBehaviour
     {
+        /// <summary>
+        /// Add the start and end events to the StartDay() and EndDay()
+        /// </summary>
         public void OnEnable()
         {
             DayLoopEvents.Instance.StartDay += StartDay;
             DayLoopEvents.Instance.EndDay += EndDay;
         }
-
+        /// <summary>
+        /// Remove the start and end events to the StartDay() and EndDay()
+        /// </summary>
         public void OnDisable()
         {
             DayLoopEvents.Instance.StartDay -= StartDay;
             DayLoopEvents.Instance.EndDay -= EndDay;
         }
-
+        /// <summary>
+        /// Event that gets called at the start of the day
+        /// </summary>
         protected abstract void StartDay();
+        /// <summary>
+        /// Event that gets called at the end of the day
+        /// </summary>
         protected abstract void EndDay();
     }
 }
