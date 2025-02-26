670577d46e028c19a60d545a626f20ce4a95a825
stenkristel <stenkristel2016@gmail.com>
2025-02-26 14:52:22 +0100

    feature[item-spawning]: adds item spawning and items jumping to a location

--- Assets/Scripts/Item/Items.cs
+++ Assets/Scripts/Item/Items.cs
@@ -6,11 +6,18 @@ namespace Item
 {
     public class Items : MonoBehaviour
     {
-        private GameObject _prefab; // visuals of the item and extra scripts
-        private int _value = 10; // the base value of the item
-        private ItemType _itemType = ItemType.Normal; // the type of item
+        [SerializeField] private GameObject visuals;        //the parent object of all visuals
+        [SerializeField] private float activationSpeed;     //the speed at which the item activates
+        [SerializeField] private float deactivationSpeed;   //the speed at which the item deactivates
+        [SerializeField] private float jumpXSpeed;          
+        [SerializeField] private float jumpYSpeed;
+        [SerializeField] private float jumpHeight;
         
-        private MinMax<int> _valuePercentage; // Minimum and Maximum value Percentage
+        private GameObject _prefab;                         // visuals of the item and extra scripts
+        private int _value = 10;                            // the base value of the item
+        private ItemType _itemType = ItemType.Normal;       // the type of item
+        
+        private MinMax<int> _valuePercentage;               // Minimum and Maximum value Percentage
         
         /// <summary>
         /// Set all item data from scriptable object
@@ -21,7 +28,7 @@ namespace Item
             _prefab = itemData.prefab;
             _value = itemData.value;
             _itemType = itemData.itemType;
-            _valuePercentage = itemData.valuePercentage;
+            _valuePercentage = itemData.valuePercentage; 
         }
         
         /// <summary>
@@ -29,5 +36,27 @@ namespace Item
         /// </summary>
         /// <returns>Returns a `MinMax<int>` with calculated values.</returns>
         public MinMax<int> CalculateValuePercent() => new MinMax<int>(_value / _valuePercentage.min, _value / _valuePercentage.max);
+
+        public void Activate()
+        {
+            visuals.SetActive(true);
+            LeanTween.scale(visuals.gameObject, Vector3.one, 0.75f).setEase(LeanTweenType.easeOutElastic);
+        }
+        public void Deactivate()
+        {
+            LeanTween.scale(visuals.gameObject, Vector3.zero, 0.50f).setEase(LeanTweenType.easeInElastic).setOnComplete(()=>visuals.SetActive(false));
+        }
+
+        public void JumpToPosition(Vector3 endPosition)
+        {
+            var distance = Vector3.Distance(transform.position, endPosition);
+            float xDuration = distance / jumpXSpeed;
+            float yDuration = distance / jumpYSpeed;
+
+            LeanTween.move(gameObject, endPosition, xDuration).setEase(LeanTweenType.easeOutQuint);
+            LeanTween.moveLocalY(visuals.gameObject, jumpHeight, yDuration).setEase(LeanTweenType.easeOutQuint).setLoopPingPong(1);
+        }
+
+        
     }
 }
