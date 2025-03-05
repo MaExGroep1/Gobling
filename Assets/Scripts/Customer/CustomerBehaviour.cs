dd738df022a12ae67015d9aaaaaeeff8aacb72d4
Brayden <91124861+braydenbos@users.noreply.github.com>
2025-03-05 12:13:02 +0100

    Feature/client movement (#43)
    * feature[ClientMovement]: Changes the movement to be more generic and array based
    
    * feature[ClientMovement]: changes MoveCustomer from IEnumerator to async void and waits for the customer to stop moving
    
    * Model[ClientMovement]:Adds Goblin With Walking Animation
    
    * Animation[All]: Adds Idle Yes No Give Take Walk Talk
    
    * feature[ClientMovement]: changes the movement and switches the template for the goblin
    
    * feature[ClientMovement]: changes the prefab to a animator and changes the rotation to a coroutine
    
    * hotfix[ClientMovemenr]: makes duplicate scene with all changes for merging makes the paths a prefab
    
    * comments[ClientMovement]: adds comments to the customerbehaviour and customermanager
    
    * style[ClientMovement]: changes the variables to be in the correct order
    
    ---------
    
    Co-authored-by: Maud <33705@ma-web.nl>

--- Assets/Scripts/Customer/CustomerBehaviour.cs
+++ Assets/Scripts/Customer/CustomerBehaviour.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using Item;
 using Trading;
 using UnityEngine;
@@ -15,26 +17,30 @@ namespace Customer
         private float _greediness; // high value will raise the goblins sell prices and lower buy prices
         private float _satisfaction =  0.5f; // how satisfied the customer is with the user
         private float _trustworthiness; // the percentage of bad items in this customer's lootTable
+        private float _speed; // the speed at which the customer moves
+        private float _turnSpeed; // the speed at which the customer turns
+        private Coroutine _rotationCoroutine; // the coroutine of the customer rotating while walking
+        private Animator _animator; // the animator of the customer
         
         private int _netWorth; // how much currency the customer has in total
         private int _income; // the amount of currency the customer earns every day 
         private readonly List<Items> _inventory = new(); // all the items the customer has
-
-        public Action OnExitShop;
-
+        
         /// <summary>
         /// Transfer all data from the customer data scriptable object to this script
         /// </summary>
         /// <param name="customerData">The Target Data to copy</param>
         public void Initialize(CustomerData customerData)
         {
-            Instantiate(customerData.prefab, transform);
+            _animator = Instantiate(customerData.prefab, transform).GetComponent<Animator>();
             gameObject.name = customerData.name;
             _lootTable = customerData.lootTable;
             _greediness = customerData.greediness=
             _trustworthiness = customerData.trustworthiness;
             _netWorth = customerData.netWorth;
             _income = customerData.income;
+            _speed = customerData.speed;
+            _turnSpeed = customerData.turnSpeed;
             for (var i = 0; i < customerData.startInventorySize; i++)
                 OnGetNewItem(_lootTable.GetRandomLoot());
         }
@@ -82,48 +88,83 @@ namespace Customer
         /// <summary>
         /// Enter the shop to barter with the player
         /// </summary>
-        /// <param name="customerEntryPoint">The position where the customer starts</param>
-        /// <param name="customerTradePoint">The target position of the customer</param>
-        /// <param name="speed">The speed at which the customer moves</param>
-        public void EnterShop(Transform customerEntryPoint,Transform customerTradePoint, float speed)
+        /// <param name="path"></param>
+        /// <param name="onComplete"></param>
+        public void EnterShop(Transform[] path, Action onComplete)
         {
-            var distance = Vector3.Distance(transform.position, customerTradePoint.position);
-            transform.position = customerEntryPoint.position;
+            var direction = path[1].position - transform.position;
+            var rotation = Quaternion.LookRotation(direction).eulerAngles;
+            _animator.SetFloat("SpeedMultiplier", _speed);
+            transform.rotation = Quaternion.Euler(rotation);
+
+            transform.position = path[0].position;
             gameObject.SetActive(true);
-            LeanTween.move(gameObject, customerTradePoint, distance / speed).setEase(LeanTweenType.easeOutQuad).setOnComplete(OnAtCounter);
+            MoveCustomer(path,onComplete);
         }
+
         /// <summary>
-        /// Leave the shop after bartering
+        /// Enter the shop to barter with the player
         /// </summary>
-        /// <param name="customerExitPoint">The position to move to to exit the shop</param>
-        /// <param name="speed">The speed at which the customer moves</param>
-        public void ExitShop(Transform customerExitPoint, float speed)
+        /// <param name="path"></param>
+        /// <param name="onComplete"></param>
+        public void ExitShop(Transform[] path, Action onComplete)
         {
-            var distance = Vector3.Distance(transform.position, customerExitPoint.position);
-            LeanTween.move(gameObject, customerExitPoint, distance / speed).setEase(LeanTweenType.easeInQuad).setOnComplete(OnShopExited);
-        }
-        /// <summary>
-        /// Gets called when the customer leaves the shop. Makes the customer inactive
-        /// </summary>
-        private void OnShopExited()
-        {
-            OnExitShop?.Invoke();
-            gameObject.SetActive(false);
+            transform.position = path[0].position;
+            MoveCustomer(path,onComplete);
         }
         
         /// <summary>
-        /// Determines the customer's action upon reaching the counter.
-        /// The customer will either attempt to buy an item or offer an item for sale.
+        /// Leave the shop after bartering
         /// </summary>
-        private void OnAtCounter()
+        /// <param name="path"></param>
+        /// <param name="recall"></param>
+        private async void MoveCustomer(Transform[] path, Action recall)
         {
-            var validItems = _inventory.Count + UserData.Instance.inventoryCount;
-            if (_inventory.Count < Random.Range(0, validItems))
-                OnTryBuyItem();
-            else
-                OnOfferItem();
-        }
+            for (var index = 1; index < path.Length; index++)
+            {
+                var point = path[index];
+                var distance = Vector3.Distance(transform.position, point.position);
+                
+                if(_rotationCoroutine != null) StopCoroutine(_rotationCoroutine);
+                _rotationCoroutine = StartCoroutine(RotateCharacter(point.position));
+                LeanTween.move(gameObject, point.position, distance / _speed);
+                
+                await Task.Delay(10);
 
-      
+                while (LeanTween.isTweening(gameObject))
+                    await Task.Delay(10); 
+            }
+            recall?.Invoke();        
+        }
+        
+        /// <summary>
+        /// Rotate the customer towards the next point in the path easing out quart
+        /// </summary>
+        /// <param name="rotateTarget">The position to rotate towards</param>
+        /// <returns></returns>
+        private IEnumerator RotateCharacter(Vector3 rotateTarget)
+        {
+            var direction = rotateTarget - transform.position;
+            var targetRotation = Quaternion.LookRotation(direction);
+    
+            var startAngle = transform.rotation.eulerAngles.y;
+            var targetAngle = targetRotation.eulerAngles.y;
+            var rotationTime = 0f;
+            const float duration = 1f;
+
+            while (rotationTime < duration)
+            {
+                rotationTime += Time.deltaTime * _turnSpeed;
+                var t = rotationTime / duration;
+                t = 1 - Mathf.Pow(1 - t, 4); 
+
+                var newY = Mathf.LerpAngle(startAngle, targetAngle, t);
+                transform.rotation = Quaternion.Euler(0, newY, 0);
+
+                yield return null;
+            }
+
+            transform.rotation = targetRotation;
+        }
     }
 }
