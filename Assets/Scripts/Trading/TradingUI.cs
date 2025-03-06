366ef9ebb7412e0a9c87f6f9a27ca5ffc2f72c0c
Tomm9000 <90690902+Tomm9000@users.noreply.github.com>
2025-03-06 12:50:10 +0100

    Feature[BarterUIBounce] cleans up code and adds comments

--- Assets/Scripts/Trading/TradingUI.cs
+++ Assets/Scripts/Trading/TradingUI.cs
@@ -12,35 +12,37 @@ namespace Trading
         [SerializeField] private GameObject uiParent; // Parent UI element for trading
         [SerializeField] private TMP_Text bidAmount; // The bid amount TextMeshPro Text
         [SerializeField] private Slider bidSlider; // The bid amount slider UI element
-
         [SerializeField] private Button makeBidButton; // Button to confirm bid
-
-        private GameObject _startPos;
-        private GameObject _targetPos;
+        [SerializeField] private Transform startPos; // The position the UI needs to start in or idle in
+        [SerializeField] private Transform targetPos; // The position the UI needs to move to 
+        
         
         /// <summary>
         /// Initializes event listeners and registers to the PawningManager's events.
         /// </summary>
         private void Awake()
         {
-            PawningManager.Instance.OnStartpawn += OnStartpawn;
-            uiParent.transform.position = _startPos.transform.position;
+            PawningManager.Instance.OnStartpawn += OnStartPawn;
+            
+            uiParent.transform.position = startPos.transform.position;
         
             bidSlider.onValueChanged.AddListener(OnBarChanged);
             makeBidButton.onClick.AddListener(OnBid);
         }
         
+        
+        // ReSharper disable Unity.PerformanceAnalysis
         /// <summary>
         /// Starts the pawn process by setting up the bid UI and displaying it.
         /// </summary>
         /// <param name="barValue">The min and max range for the bid slider</param>
         /// <param name="baseValue">The initial bid value</param>
-        public void OnStartpawn(MinMax<int> barValue, int baseValue)
+        public void OnStartPawn(MinMax<int> barValue, int baseValue)
         {
             SetBidSlider(barValue, baseValue);
             MoveIn();
-            
-        }
+        }   
+        
         
         /// <summary>
         /// Updates the bid amount text when the slider value changes.
@@ -48,43 +50,50 @@ namespace Trading
         /// <param name="barValue">The current slider value</param>
         private void OnBarChanged(float barValue) => bidAmount.text = Mathf.Ceil(bidSlider.value).ToString();
     
+        
         /// <summary>
         /// Configures the bid slider's minimum, maximum, and initial value.
         /// </summary>
         /// <param name="barValue">The min and max range for the slider</param>
         /// <param name="itemValue">The default value to set on the slider</param>
-        public void SetBidSlider(MinMax<int> barValue, int itemValue)
+        private void SetBidSlider(MinMax<int> barValue, int itemValue)
         {
             bidSlider.minValue = barValue.min;
             bidSlider.maxValue = barValue.max;
             bidSlider.value = itemValue;
         }
 
+        
         /// <summary>
         /// Sends the final bid amount to the PawningManager for processing.
         /// </summary>
         private void OnBid() => PawningManager.Instance.CheckBid((int)bidSlider.value);
 
+        
         /// <summary>
         /// Hides the trading UI after the bid process finishes.
         /// </summary>
         public void OnBidFinish()
         {
+            //!TODO Check if bid is accepted or denied
             
-            //uiParent.SetActive(false);
+            MoveAway();
         }
-
-        private void MoveIn()
-        {
-            DoSetActive(true);
-            LeanTween.move(uiParent, new Vector3(960, 540, 0), 3).setEase(LeanTweenType.easeOutBack);
-        }
-
-        private void MoveOut()
-        {
-            LeanTween.move(uiParent, _startPos.transform.position, 2).setEase(LeanTweenType.easeInBack);
-        }
-
+        
+        
+        /// <summary>
+        /// Moves UI into the scene when the customer interacts
+        /// </summary>
+        private void MoveIn() => LeanTween.move(uiParent, targetPos.transform.position, 3).setEase(LeanTweenType.easeOutBack);
+        
+        
+        /// <summary>
+        /// Moves UI away after it's done being used by the player.
+        /// </summary>
+        private void MoveAway() => LeanTween.move(uiParent, startPos.transform.position, 2).setEase(LeanTweenType.easeInBack);
+        
+        
+        //!TODO find usefulness
         private void DoSetActive(bool isActive) => uiParent.SetActive(isActive);
     }
 }
