0ecfe4f8f59386e9274f735bb6a931996c9d4032
stenkristel <stenkristel2016@gmail.com>
2025-03-11 13:13:11 +0100

    feature[item-loottable]: Adds more items to loottable

--- Assets/Scripts/testing/SpawnTestItem.cs
+++ Assets/Scripts/testing/SpawnTestItem.cs
@@ -6,30 +6,26 @@ using UnityEngine;
 
 public class SpawnTestItem : MonoBehaviour
 {
-    [SerializeField] private GameObject itemPrefab;
-    [SerializeField] private GameObject jumpPosition;
     [SerializeField] private GameObject spawnPoint;
     [SerializeField] private LootTable lootTable;
     
-    private GameObject _spawnedItem;
-    private Items _item;
-    private int _currentItem = 0;
+    private int _currentItem = -1;
     private GameObject _theItem;
-
-    void Start()
-    {
-        _spawnedItem = Instantiate(itemPrefab, spawnPoint.transform.position, Quaternion.identity);
-        _item = _spawnedItem.GetComponent<Items>();
-    }
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.RightArrow))
         {
             if (_theItem != null) Destroy(_theItem);
-            _theItem = Instantiate(lootTable.GetItem(_currentItem).prefab, spawnPoint.transform.position, Quaternion.identity);
             _currentItem++;
             if (lootTable.GetItem(_currentItem) == null) _currentItem = 0;
+            _theItem = Instantiate(lootTable.GetItem(_currentItem).prefab, spawnPoint.transform.position, Quaternion.identity);
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            if (_theItem != null) Destroy(_theItem);
+            _currentItem--;
+            _theItem = Instantiate(lootTable.GetItem(_currentItem).prefab, spawnPoint.transform.position, Quaternion.identity); 
         }
     }
 }
