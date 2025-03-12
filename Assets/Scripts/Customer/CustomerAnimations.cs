e399cbc15eb24458c1c6429bc7ceeac8ec150d09
braydenbos <bosbrayden@gmail.com>
2025-03-12 12:40:17 +0100

    feature[CustomerAnimator]: Switches functionality from jump time to customerAnimations

--- Assets/Scripts/Customer/CustomerAnimations.cs
+++ Assets/Scripts/Customer/CustomerAnimations.cs
@@ -6,9 +6,21 @@ namespace Customer
     public class CustomerAnimations : MonoBehaviour
     {
         private Animator _animator;
+        public bool jump { get; private set; }
 
+        public bool hasGotItem { get; private set; }
+        
+        
         private void Awake() => _animator = GetComponent<Animator>();
 
+        public void StartJump() => jump = true;
+
+        public void StopJump() => jump = false;
+        
+        public void GotItem() => hasGotItem = true;
+        
+        public void GaveItem() => hasGotItem = false;
+        
         public void SetSpeedFloat(float value) => _animator.SetFloat("SpeedMultiplier", value);
         
         public void TriggerAtCounter() => _animator.SetTrigger("AtCounter");
