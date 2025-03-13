83864cc9a5d68f3dd6b63c1d4debc8dff5d4476a
braydenbos <bosbrayden@gmail.com>
2025-03-13 17:01:05 +0100

    comment[MainMenu]: adds comments to the main menu scripts

--- Assets/Scripts/Difficulty/DifficultyUI.cs
+++ Assets/Scripts/Difficulty/DifficultyUI.cs
@@ -8,16 +8,22 @@ namespace Difficulty
 {
     public class DifficultyUI : MonoBehaviour
     {
-        [SerializeField] private GameObject difficultyUI;
-        [SerializeField] private Button normal;
-        [SerializeField] private Button easy;
+        [SerializeField] private GameObject difficultyUI;   // the parent of the main menu
+        [SerializeField] private Button normal;             // the normal difficulty button
+        [SerializeField] private Button easy;               // the easy difficulty button
 
+        /// <summary>
+        /// Starts listening to the buttons
+        /// </summary>
         private void Awake()
         {
             easy.onClick.AddListener(StartEasy);
             normal.onClick.AddListener(StartNormal);
         }
-
+        
+        /// <summary>
+        /// Deactivates the UI and starts the game
+        /// </summary>
         private void StartNormal()
         {
             difficultyUI.SetActive(false);
@@ -25,6 +31,9 @@ namespace Difficulty
             DifficultyManager.Instance.SetDifficultyNormal();
         }
 
+        /// <summary>
+        /// Deactivates the UI and starts the game
+        /// </summary>
         private void StartEasy()
         {
             difficultyUI.SetActive(false);
--- Assets/Scripts/Difficulty/Reset.cs
+++ Assets/Scripts/Difficulty/Reset.cs
@@ -7,9 +7,16 @@ namespace Difficulty
 {
     public class Reset : MonoBehaviour
     {
-        [SerializeField] private float waitTime;
+        [SerializeField] private float waitTime;    // the time between going to the main menu
+        
+        /// <summary>
+        /// Waits to go to the main menu
+        /// </summary>
         private void Awake() => StartCoroutine(WaitToLoad());
         
+        /// <summary>
+        /// Waits to go to the main menu
+        /// </summary>
         private IEnumerator WaitToLoad()
         {
             yield return new WaitForSeconds(waitTime);
