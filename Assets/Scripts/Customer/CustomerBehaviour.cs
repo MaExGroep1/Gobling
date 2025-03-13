ea004c557cb12f1876c75ebbdaed907add2478a0
Sten <90691070+stenkristel@users.noreply.github.com>
2025-03-13 14:15:49 +0100

    Feature/item jump animation sync (#90)
    * feature[item-jump-animation]: deletes old animation triggers and adds some new ones
    
    * fix[item-jump-animation]; temporary UI movement fix
    
    * fix[tiem-jump-animation]: fixes UI not moving properly into the screen
    
    * fix[item-jump-animation-fix]: reverts unneccecery fix
    
    * feature[item-jump-animation-sync]: Updates the proper location of where the customers hand is and triggers movement at the right times
    
    * fix[item-jump-animation-sync]: Sets all base positions for items to (0, 0, 0)

--- Assets/Scripts/Customer/CustomerBehaviour.cs
+++ Assets/Scripts/Customer/CustomerBehaviour.cs
@@ -162,7 +162,7 @@ namespace Customer
             _animator.CantJump();
             _animator.TriggerGiveTake();
             yield return new WaitUntil(() => _animator.itemCanJump);
-            // TODO: Add jump here
+            ItemManager.Instance.ItemJumpAndDisable(PawningManager.Instance.OfferItem, ItemManager.Instance.ItemCustomerJumpLocation);
             yield return new WaitUntil(() => _animator.canLeaveShop);
         }
         /// <summary>
@@ -187,6 +187,8 @@ namespace Customer
         /// </summary>
         private void OnOfferItem()
         {
+            Debug.Log("Customer wants to sell an item");
+
             var item = _inventory[Random.Range(0, _inventory.Count - 1)];
             
             _animator.TriggerGiveTake();
@@ -201,6 +203,7 @@ namespace Customer
         /// </summary>
         private void OnTryBuyItem()
         {
+            Debug.Log("Customer wants to buy an item");
             PawningManager.Instance.RequestUserItem(this);
         }
 
@@ -288,7 +291,7 @@ namespace Customer
         {
             _animator.CantJump();
             yield return new WaitUntil(() => _animator.itemCanJump);
-            //TODO: add item jump
+            ItemManager.Instance.ItemEnableAndJump(PawningManager.Instance.OfferItem, ItemManager.Instance.ItemCounterJumpLocation, ItemManager.Instance.ItemCustomerJumpLocation);
         }
     }
 }
