1a823b13e327e24c23f9f6616c3a67f1458daad7
braydenbos <bosbrayden@gmail.com>
2025-03-13 15:12:33 +0100

    feature[BuyingSellingIndicator]: Adds text at the top of the screen to show weather the player is selling or buying

--- Assets/Scripts/Trading/TradingUI.cs
+++ Assets/Scripts/Trading/TradingUI.cs
@@ -12,23 +12,32 @@ namespace Trading
 {
     public class TradingUI : MonoBehaviour
     {
+        [Header("Default")]
         [SerializeField] private GameObject uiParent;               // parent UI element for trading
         [SerializeField] private TMP_Text bidAmount;                // the bid amount TextMeshPro Text
         [SerializeField] private Slider bidSlider;                  // the bid amount slider UI element
         [SerializeField] private Button makeBidButton;              // button to confirm bid
         [SerializeField] private Button rejectButton;               // button to reject bid
         
+        [Header("Positions")]
         [SerializeField] private Transform bottomPosition;          // the position the UI needs to start in or idle in
         [SerializeField] private Transform topPosition;             // the position the button needs to move to
+        [SerializeField] private Transform overTopPosition;         // the position the button needs to move to
         
+        [Header("Easy mode text")]
         [SerializeField] private TMP_Text satisfaction;             // the easy satisfaction meter
         [SerializeField] private TMP_Text baseValue;                // the easy base value
         
+        [Header("Base value buy")]
         [SerializeField] private Button baseValueBuyButton;         // button to buy the base value
         [SerializeField] private GameObject baseValueBuyObject;     // the text area where baseValueBuy text is displayed
         [SerializeField] private TMP_Text baseValueBuy;             // the paid base value text
         [SerializeField] private TMP_Text baseValueBuyButtonText;   // the text on the baseValueBuy button
-        [SerializeField] private int baseValueBuyAmount;            // the amount of currency it costs to see the base value  
+        [SerializeField] private int baseValueBuyAmount;            // the amount of currency it costs to see the base value
+        
+        [Header("Buying or selling")]
+        [SerializeField] private GameObject buyingOrSellingParent;
+        [SerializeField] private TMP_Text buyingOrSellingText;
 
         
         /// <summary>
@@ -53,11 +62,13 @@ namespace Trading
         /// <param name="barValue">The initial bid value</param>
         /// <param name="itemBaseValue">The base value of the item</param>
         /// <param name="customer">The current customer</param>
-        private void OnStartPawn(MinMax<int> barValues, int barValue, int itemBaseValue,CustomerBehaviour customer)
+        /// <param name="topText">Text to show at the top of the screen</param>
+        private void OnStartPawn(MinMax<int> barValues, int barValue, int itemBaseValue, CustomerBehaviour customer, string topText)
         {
             baseValueBuyObject.SetActive(false);
             baseValueBuyButton.interactable = true;
             baseValueBuyButtonText.text = $"Get base value: {baseValueBuyAmount}$";
+            buyingOrSellingText.text = topText;
                 
             baseValue.text = $"Base value: {itemBaseValue}";
             baseValueBuy.text = $"Base value: {itemBaseValue}";
@@ -110,6 +121,7 @@ namespace Trading
         /// </summary>
         private void MoveIn()
         {
+            LeanTween.move(buyingOrSellingParent, topPosition.transform.position, 3).setEase(LeanTweenType.easeOutBack);
             LeanTween.move(uiParent, topPosition.transform.position, 3).setEase(LeanTweenType.easeOutBack);
         }
         
@@ -118,6 +130,7 @@ namespace Trading
         /// </summary>
         private void MoveAway()
         {
+            LeanTween.move(buyingOrSellingParent, overTopPosition.transform.position, 3).setEase(LeanTweenType.easeInBack);
             LeanTween.move(uiParent, bottomPosition.transform.position, 3).setEase(LeanTweenType.easeInBack);
         }
 
