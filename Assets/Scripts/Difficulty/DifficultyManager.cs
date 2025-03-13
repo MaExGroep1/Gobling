16a62d34438813803625eaa9de0a162af7557ffb
braydenbos <bosbrayden@gmail.com>
2025-03-13 13:47:53 +0100

    feature[EasyMode]: Adds buy base value button and updates the style

--- Assets/Scripts/Difficulty/DifficultyManager.cs
+++ Assets/Scripts/Difficulty/DifficultyManager.cs
@@ -4,18 +4,30 @@ namespace Difficulty
 {
     public class DifficultyManager : MonoBehaviour
     {
-        [SerializeField] private GameObject satisfaction, baseValue;
+        [SerializeField] private GameObject 
+            satisfaction,               // the customer satisfaction display
+            baseValue,                  // the item base value display
+            baseValueBuyButton;         // the paid for base value button
 
+        /// <summary>
+        /// Sets the difficulty to easy
+        /// </summary>
         public void SetDifficultyEasy()
         {
             satisfaction.SetActive(true);
             baseValue.SetActive(true);
+            baseValueBuyButton.SetActive(false);
         }
+        
+        /// <summary>
+        /// Sets the difficulty to normal
+        /// </summary>
 
         public void SetDifficultyNormal()
         {
             satisfaction.SetActive(false);
             baseValue.SetActive(false);
+            baseValueBuyButton.SetActive(true);
         }
     }
 }
